8756a12331933bcdada934276a329b37c81daa52
abhijithpr08 <abhith628@gmail.com>
2025-12-16 22:06:35 +0530

    add page route in routes folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/authRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import { fileURLToPath } from "url";
 import path from "path";
+import pageRoutes from "./routes/pageRoutes.js"
 
 
 dotenv.config();
@@ -12,31 +13,12 @@ connectDB();
 
 const app = express();
 
-//Fix __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "register.html"));
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
-});
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 //  static files 
 app.use(express.static("public"));
+app.use("/",pageRoutes)
 
 //  routes 
 app.use("/api/auth", authRoutes);
